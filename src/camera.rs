016fc2a3caef07520a067727327a4977e131e816
milkyskies <ryan@websculptors.net>
2024-06-11 14:28:10 +0900

    Add organisms

---
+++ src/camera.rs
@@ -0,0 +1,103 @@
+use bevy::{input::mouse::MouseWheel, prelude::*, window::PrimaryWindow};
+
+#[derive(Component)]
+struct MainCamera;
+
+#[derive(Component)]
+struct CameraZoom(f32);
+
+#[derive(Component)]
+struct CameraPan {
+    is_panning: bool,
+    last_position: Vec2,
+}
+
+#[derive(Resource, Default)]
+struct CursorWorldPosition(Vec2);
+
+#[derive(Resource, Default)]
+struct CursorWindowPosition(Vec2);
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(CursorWorldPosition::default())
+            .insert_resource(CursorWindowPosition::default())
+            .add_systems(Startup, setup_camera)
+            .add_systems(Update, (handle_zoom, handle_panning, cursor_system));
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2dBundle::default(),
+        MainCamera,
+        CameraZoom(1.0),
+        CameraPan {
+            is_panning: false,
+            last_position: Vec2::new(0.0, 0.0),
+        },
+    ));
+}
+
+fn handle_zoom(
+    mut query: Query<(&mut Transform, &mut CameraZoom), With<Camera>>,
+    mut scroll_evr: EventReader<MouseWheel>,
+) {
+    for ev in scroll_evr.read() {
+        for (mut transform, mut zoom) in &mut query {
+            zoom.0 *= 1.0 - ev.y * 0.1;
+            zoom.0 = zoom.0.clamp(0.1, 10.0);
+            transform.scale = Vec3::splat(zoom.0);
+        }
+    }
+}
+
+fn handle_panning(
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    cursor_window_position: Res<CursorWindowPosition>,
+    mut query: Query<(&mut Transform, &mut CameraPan), With<MainCamera>>,
+) {
+    let (mut transform, mut camera_pan) = query.single_mut();
+
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        camera_pan.is_panning = true;
+        camera_pan.last_position = cursor_window_position.0;
+    }
+
+    if mouse_button_input.just_released(MouseButton::Left) {
+        camera_pan.is_panning = false;
+    }
+
+    if camera_pan.is_panning {
+        for event in cursor_moved_events.read() {
+            let delta = event.position - camera_pan.last_position;
+            transform.translation.x -= delta.x;
+            transform.translation.y += delta.y;
+            camera_pan.last_position = event.position;
+        }
+    }
+}
+
+fn cursor_system(
+    mut cursor_world_position: ResMut<CursorWorldPosition>,
+    mut cursor_window_position: ResMut<CursorWindowPosition>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let window = window_query.single();
+
+    if let Some(cursor_position) = window.cursor_position() {
+        cursor_window_position.0 = cursor_position;
+
+        if let Some(world_position) = camera
+            .viewport_to_world(camera_transform, cursor_position)
+            .map(|ray| ray.origin.truncate())
+        {
+            cursor_world_position.0 = world_position;
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use bevy::{
+    input::mouse::MouseWheel, prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow,
+};
+
+mod camera;
+mod stepping;
+
+const ORGANISM_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const WORLD_SIZE: Vec2 = Vec2::new(1024.0, 1024.0);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            stepping::SteppingPlugin::default()
+                .add_schedule(Update)
+                .add_schedule(FixedUpdate)
+                .at(Val::Percent(35.0), Val::Percent(50.0)),
+        )
+        .add_plugins(camera::CameraPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(
+            FixedUpdate,
+            (apply_velocity)
+                // `chain`ing systems together runs them in order
+                .chain(),
+        )
+        .add_systems(Update, (bevy::window::close_on_esc))
+        .run();
+}
+
+#[derive(Component)]
+struct Organism;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let organism_mesh_handle = meshes.add(Circle::default());
+    let organism_material_handle = materials.add(ORGANISM_COLOR);
+
+    // Balls
+    (0..5000).for_each(|_| {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: organism_mesh_handle.clone().into(),
+                material: organism_material_handle.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    rand::random::<f32>() * WORLD_SIZE.x - WORLD_SIZE.x / 2.0,
+                    rand::random::<f32>() * WORLD_SIZE.y - WORLD_SIZE.y / 2.0,
+                    0.0,
+                ))
+                .with_scale(Vec3::new(
+                    rand::random::<f32>() * 0.5 + 0.5,
+                    rand::random::<f32>() * 0.5 + 0.5,
+                    1.0,
+                )),
+                ..default()
+            },
+            Organism,
+            Velocity(Vec2::new(
+                rand::random::<f32>() * 2.0 - 1.0,
+                rand::random::<f32>() * 2.0 - 1.0,
+            )),
+        ));
+    });
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * time.delta_seconds();
+        transform.translation.y += velocity.y * time.delta_seconds();
+    }
+}
